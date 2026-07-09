3c49c6e4a7d40ba9bc65c6d84205a7412d827a16
Umer Bigfolio <umer.masood@bigfolio.co>
2026-07-10 01:09:01 +0500

    remaining-sections-code

--- assets/section-reviews-slider.js
+++ assets/section-reviews-slider.js
@@ -7,14 +7,20 @@ const gap = parseInt(slider.dataset.gap, 10);
 new Swiper(slider, {
   slidesPerView: mobile,
   spaceBetween: gap,
+  centeredSlides: false,
+  spaceBetween: 12,
+  loop: true,
+  slidesOffsetBefore: 0,
+  slidesOffsetAfter: 0,
   breakpoints: {
     768: {
       slidesPerView: tablet,
       spaceBetween: gap
     },
     992: {
-      slidesPerView: desktop,
-      spaceBetween: gap
+      slidesPerView: 3.2,
+      loop: true,
+      spaceBetween: gap,
     }
   }
 });
